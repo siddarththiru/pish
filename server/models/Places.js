6a472b63bdec37e6e381c9937470e3d433c13697
Siddarth Thirunavukkarasu <siddarththiru@gmail.com>
2024-03-21 17:50:37 +0000

    schema for use in the places collection created

--- server/models/Places.js
+++ server/models/Places.js
@@ -5,7 +5,51 @@ const PlaceSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  address: {
+  location: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  timings: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: {
+      location1: String,
+      location2: String,
+      location3: String,
+    },
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  discounts: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: {
+      link1: String,
+      link2: String,
+      link3: String,
+    },
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  mealOptions: {
+    type: String,
+    required: true,
+  },
+  diningOptions: {
     type: String,
     required: true,
   },
