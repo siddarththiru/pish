3e4e69956d3d2c2e9b59dcb2346df84698ee0824
Siddarth Thirunavukkarasu <siddarththiru@gmail.com>
2024-03-25 09:39:04 +0000

    routing fixes and added background image

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import AddPlaces from "./pages/AddPlaces";
+import PlaceDetails from "./pages/PlaceDetails";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path="/search" element={<Search />} />
             <Route path="/add-places" element={<AddPlaces />} />
+            <Route path="/place-details" element={<PlaceDetails />} />
           </Routes>
         </BrowserRouter>
       </div>
