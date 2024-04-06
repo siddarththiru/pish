97e7ab99b942d65c0a7148c93259706a02d45311
Siddarth Thirunavukkarasu <siddarththiru@gmail.com>
2024-04-06 11:23:34 +0100

    css fixes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,7 @@
 import React from "react";
-
 import Navbar from "../components/Navbar";
 import ImageSlider from "../components/ImageSlider";
 import { useLocation, useNavigate } from "react-router-dom";
-
 import SuggestedPlaces from "../components/SuggestedPlaces";
 
 function Home({ setActivePlace, placeList }) {
