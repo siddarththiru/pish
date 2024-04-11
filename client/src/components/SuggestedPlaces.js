5218e38989e62d536c8934a470ee8dc54ff6f867
Siddarth Thirunavukkarasu <siddarththiru@gmail.com>
2024-04-11 14:35:38 +0100

    home page fixes- suggested places links now work

--- client/src/components/SuggestedPlaces.js
+++ client/src/components/SuggestedPlaces.js
@@ -1,34 +1,49 @@
 import React from "react";
 import { Flex, Box, Text, Heading, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
-function SuggestedPlaces({ placeList }) {
+function SuggestedPlaces({ placeList, setActivePlace }) {
   return (
     <Flex color="white" flexDir="column" padding="30px 100px">
       <Heading>Suggested Places</Heading>
       <Flex flexDir="row" flexWrap="wrap" padding="20px">
         {placeList.map((place, index) => (
-          <Flex key={index} height="100%" paddingRight="20px" flexDir="column">
-            {/*Bug fix*/}
-            {place.images && place.images.link1 && (
-              <Image
-                width="250px"
-                height="200px"
-                objectFit="cover"
-                borderRadius="10px"
-                src={place.images.link1}
-              />
-            )}
+          <Link
+            to={{
+              pathname: `/place-details/${placeList.id}`,
+            }}
+          >
             <Flex
-              padding="5px"
-              borderRadius="10px"
-              style={{
-                background:
-                  "linear-gradient(to right, #383E56F2, #383E56CC, #383E5600)",
+              key={index}
+              height="100%"
+              paddingRight="20px"
+              flexDir="column"
+              onClick={() => {
+                setActivePlace(place);
               }}
             >
-              <Text>{place.name}</Text>
+              {/*Bug fix*/}
+              {place.images && place.images.link1 && (
+                <Image
+                  width="250px"
+                  height="200px"
+                  objectFit="cover"
+                  borderRadius="10px"
+                  src={place.images.link1}
+                />
+              )}
+              <Flex
+                padding="5px"
+                borderRadius="10px"
+                style={{
+                  background:
+                    "linear-gradient(to right, #383E56F2, #383E56CC, #383E5600)",
+                }}
+              >
+                <Text>{place.name}</Text>
+              </Flex>
             </Flex>
-          </Flex>
+          </Link>
         ))}
       </Flex>
     </Flex>
